f111fc2d6d72389ff4b9542cd648be01c7ec295c
Zaedul Islam <islam.zaedul@gmail.com>
2022-03-01 19:22:00 -0500

    Update Determine the Shape of NPZ file.py

--- Python/NumPy/Determine the Shape of NPZ file.py
+++ Python/NumPy/Determine the Shape of NPZ file.py
@@ -1,29 +1,27 @@
 from numpy import load
 import numpy as np
-
+import zipfile
 
 customDataset = load('data_2d_custom_myvideos.npz', allow_pickle=True)['positions_2d'].item()
 humanDataset = load('data_2d_h36m_detectron_ft_h36m.npz', allow_pickle=True)['positions_2d'].item()
 
 
-def determineShapeOfNPZ(datasetName, dataset):
+def findShape(datasetName, dataset):
     print("Dataset: ",datasetName)
 
     for subject, subjectValue in dataset.items():
         # subject -> 'S1', 'S2', etc.
         for action, actionValue in subjectValue.items():
             # action -> 'Directions 1', 'Directions', etc.        
+            print(action, end=': ')
             for value in actionValue:
-                print("Type: ", type(value))
-                print(value.shape)
-                break
-            break
+                # print("Type: ", type(value))
+                print(value.shape, end=' | ')
+                # break
+            print('\n')
+            # break
         break
 
 findShape('Custom', customDataset)
 findShape('Human3.6M', humanDataset)
-
-
-
-
 
