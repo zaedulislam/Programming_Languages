3dadd3cffa434a9066b1085da3f1b7400f8f74ee
Zaedul Islam <islam.zaedul@gmail.com>
2022-11-02 21:45:31 -0400

    Update Overflow.cpp

--- C++/Overflow.cpp
+++ C++/Overflow.cpp
@@ -5,8 +5,75 @@
 
 #include <iostream>
 #include <cmath>
+#include <limits.h>
 
 using namespace  std;
+
+bool checkIntegerOverflowForAddition(int a, int b){
+    // `a + b` would overflow
+    if (b >= 0 && a > INT_MAX - b){
+        return true;
+    }
+
+    return false;
+}
+
+
+bool checkIntegerUnderflowForAddition(int a, int b){
+    // `a + b` would underflow
+    if (b < 0 && a < INT_MIN - b){
+        return true;
+    }
+
+    return  false;
+}
+
+
+bool checkIntegerOverflowForSubtraction(int a, int b){
+    // `a - b` would overflow
+    if (b < 0 && a > INT_MAX + b){
+        return true;
+    }
+
+    return false;
+}
+
+
+bool checkIntegerUnderflowForSubtraction(int a, int b){
+    // `a - b` would underflow
+    if (b > 0 && a > INT_MIN + b){
+        return true;
+    }
+
+    return false;
+}
+
+
+bool checkIntegerOverflowForMultiplication(int a, int b){
+    // `a * b` can overflow
+    if (a == -1 && b == INT_MIN){
+        return true;
+    }
+
+    // `a * b` (or `a / b`) can overflow
+    if (b == -1 && a == INT_MIN) {
+        return true;
+    }
+    // `a * b` would overflow
+    if (b != 0 && a > INT_MAX / b) {
+        return true;
+    }
+
+    return false;
+}
+
+
+bool checkIntegerUnderflowForMultiplication(int a, int b){
+    // `a * b` would underflow
+    if (b != 0 && a < INT_MIN / b)
+        return false;
+}
+
 
 int main() {
     int a = 5, b = 4;
